764c44d73b7898536a4b3dac640338b54643153e
Blake Loring <blake_l@parsed.uk>
2018-03-23 14:01:53 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone git@github.com:jawline/vim.git temp_vim
+git clone https://github.com/jawline/vim.git temp_vim
 cp -fr ./temp_vim/.vimrc ~/
 cp -fr ./temp_vim/.vim/ ~/
 rm -rf temp_vim/
