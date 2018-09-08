381d1e5b737fe16042e4521ee85c3d11443b17ef
Blake Loring <blake_l@parsed.uk>
2018-09-08 08:16:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 git clone https://github.com/jawline/vim.git temp_vim
 cp -fr ./temp_vim/.vimrc ~/
 cp -fr ./temp_vim/.vim/ ~/
+cp -fr ./temp_vim/.tmux.conf ~/
 rm -rf temp_vim/
 
