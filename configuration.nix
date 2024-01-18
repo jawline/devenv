046fc9b5e410be62ff04c36658cf1a109618dc65
Blake Loring <blake@parsed.uk>
2024-01-18 11:04:42 +0800

    Split home and system config

--- configuration.nix
+++ configuration.nix
@@ -65,57 +65,6 @@ in {
     packages = with pkgs; [ firefox ];
   };
 
-  home-manager.users.blake = { lib, config, ... }: {
-    home.stateVersion = "23.11";
-
-    home.packages = [
-      # Dev
-      pkgs.ripgrep
-      pkgs.tmux
-      pkgs.wget
-      pkgs.curl
-      pkgs.gcc
-      pkgs.git
-      pkgs.rustup
-      pkgs.ocaml
-      pkgs.opam
-      pkgs.gnumake
-      pkgs.gnutar
-      pkgs.gnupatch
-      pkgs.gzip
-
-      # zsh 
-      pkgs.fzf
-      pkgs.zinit
-    ];
-
-    programs.git = {
-      enable = true;
-      userName = "jawline";
-      userEmail = "blake@parsed.uk";
-    };
-
-    home.file.".zshrc".source = ./etc/zshrc;
-    home.file.".vimrc".source = ./etc/vimrc;
-    home.file.".tmux.conf".source = ./etc/tmux.conf;
-    home.file.".config/i3/config".source = ./etc/i3/config;
-
-    home.activation = {
-
-      initializeOcaml = lib.hm.dag.entryAfter [ "installPackages" ] ''
-        export PATH="${config.home.path}/bin:$PATH"
-        opam init -y
-        opam update -y
-        opam install dune core async hardcaml -y
-      '';
-
-      initializeRust = lib.hm.dag.entryAfter [ "installPackages" ] ''
-        	export PATH="${config.home.path}/bin:$PATH"
-                rustup default nightly
-                rustup update
-      '';
-    };
-  };
-
+  home-manager.users.blake = import ./home.nix;
   system.stateVersion = "23.11";
 }
