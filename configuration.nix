6d037fea4718a44c56d6c64b060a2b87a41b88b4
Blake Loring <blake@parsed.uk>
2024-09-04 01:12:01 +0800

    Add steam

--- configuration.nix
+++ configuration.nix
@@ -61,6 +61,14 @@ in {
   users.defaultUserShell = pkgs.zsh;
   environment.variables = { EDITOR = "vim"; };
 
+  # Video Games
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   # Display
   services.xserver = {
 
